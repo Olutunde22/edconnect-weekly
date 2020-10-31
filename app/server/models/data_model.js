1b742df5fb06f4bb33811784bd7d06d927da7130
Olutunde <solabi.tunde@gmail.com>
2020-10-31 20:03:15 +0100

    Added initial JavaScript files to project Explorer

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -34,38 +34,38 @@ class DataModel {
                 if(obj.firstname && obj.firstname.length > 0){
                     allData[i].firstname = obj.firstname
                     return true
-                } if(obj.lastname && obj.lastname.length > 0){
+                }else if(obj.lastname && obj.lastname.length > 0){
                     allData[i].lastname = obj.lastname
                     return true
-                } if(obj.email && obj.email.length > 0){
+                }else if(obj.email && obj.email.length > 0){
                     allData[i].email = obj.email
                     return true
-                } if(obj.password && obj.password.length > 0){
+                }else if(obj.password && obj.password.length > 0){
                     allData[i].password = obj.password
                     return true
-                } if(obj.matricNumber && obj.matricNumber.length > 0){
+                }else if(obj.matricNumber && obj.matricNumber.length > 0){
                     allData[i].matricNumber = obj.matricNumber
                     return true
-                } if(obj.program && obj.program.length > 0){
+                }else if(obj.program && obj.program.length > 0){
                     allData[i].program = obj.program
                     return true
-                } if(obj.graduationYear && obj.graduationYear.length > 0){
+                }else if(obj.graduationYear && obj.graduationYear.length > 0){
                     allData[i].graduationYear = obj.graduationYear
                     return true
-                } if(obj.name && obj.name.length > 0){
+                }else if(obj.name && obj.name.length > 0){
                     allData[i].name = obj.name
                     return true
-                } if(obj.abstract && obj.abstract.length > 0 ){
+                }else if(obj.abstract && obj.abstract.length > 0 ){
                     allData[i].abstract = obj.abstract
                     return true
-                } if(obj.authors && obj.authors.length > 0){
+                }else if(obj.authors && obj.authors.length > 0){
                     allData[i].authors = obj.authors
                     console.log(obj.tags)
                     return true
-                }if(obj.tags && obj.tags.length > 0){
+                }else if(obj.tags && obj.tags.length > 0){
                     allData[i].tags = obj.tags
                     return true
-                }if(obj.createdBy && obj.createdBy.length > 0){
+                }else if(obj.createdBy && obj.createdBy.length > 0){
                     allData[i].createdBy = obj.createdBy
                     return true
                 }
@@ -84,6 +84,8 @@ class DataModel {
                     allData[i] = allData[allData.length - 1]
                     allData.pop()
                     return true;
+                } else{
+                    return  false;
                 }
             }
         } else{
