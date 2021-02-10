eb8fc7d54925f2b3372c7a0742dc61e837be8c76
Olutunde <solabi.tunde@gmail.com>
2021-02-10 15:03:54 +0100

    completed week 8 Exercise

--- app/views/shared/Header.jsx
+++ app/views/shared/Header.jsx
@@ -3,10 +3,6 @@ import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
 
 const Header = ({firstname}) => {
-	const [username, setUsername] = useState('');
-	useEffect(() => {
-		setUsername(firstname)
-	}, []);
 	return (
 		<Navbar bg="primary" variant="dark" className="justify-content-between" expand="lg">
 			<Navbar.Brand href="/">Project Explorer</Navbar.Brand>
@@ -23,7 +19,7 @@ const Header = ({firstname}) => {
 				{username !== undefined ? (
 					<Nav className="justify-content-end">
 						<Nav.Link href="/logout"eventKey="">Logout</Nav.Link>
-						<Nav.Link id="username"href="" disabled>Hi, {username} </Nav.Link>	
+						<Nav.Link id="username"href="" disabled>Hi, {firstname} </Nav.Link>	
 					</Nav>
 				) : (
 					<Nav className="justify-content-end">
